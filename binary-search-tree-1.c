fc0476aff132a19736e49573725c270d1d07a638
goodyoung <goodyoung0615@gmail.com>
2024-05-28 13:39:33 +0900

    Update: code comment

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -111,11 +111,9 @@ int main()
 }
 
 int initializeBST(Node** h) {
-
 	/* if the tree is not empty, then remove all allocated nodes from the tree*/
 	if(*h != NULL)
 		freeBST(*h);
-
 	/* create a head node */
 	*h = (Node*)malloc(sizeof(Node));
 	(*h)->left = NULL;	/* root */
@@ -129,9 +127,9 @@ int initializeBST(Node** h) {
 void inorderTraversal(Node* ptr)
 {
 	if (ptr){
-		inorderTraversal(ptr->left);
+		inorderTraversal(ptr->left); // 현재 노드 기준으로 왼쪽 노드 순회
 		printf(" [%d] ", ptr->key); // 현재 노드의 키 값을 출력
-		inorderTraversal(ptr->right);
+		inorderTraversal(ptr->right); // 현재 노드 기준으로 오른쪽 노드 순회
 	}
 
 }
@@ -140,8 +138,8 @@ void preorderTraversal(Node* ptr)
 {
 	if (ptr){
 		printf(" [%d] ", ptr->key); // 현재 노드의 키 값을 출력
-		preorderTraversal(ptr->left);
-		preorderTraversal(ptr->right);
+		preorderTraversal(ptr->left); // 현재 노드 기준으로 왼쪽 노드 순회
+		preorderTraversal(ptr->right); // 현재 노드 기준으로 오른쪽 노드 순회
 	}
 
 }
@@ -149,23 +147,24 @@ void preorderTraversal(Node* ptr)
 void postorderTraversal(Node* ptr)
 {
 	if (ptr){
-		postorderTraversal(ptr->left);
-		postorderTraversal(ptr->right);
+		postorderTraversal(ptr->left); // 현재 노드 기준으로 왼쪽 노드 순회
+		postorderTraversal(ptr->right); // 현재 노드 기준으로 오른쪽 노드 순회
 		printf(" [%d] ", ptr->key); // 현재 노드의 키 값을 출력
 	}
-
 
 }
 
 
 int insert(Node* head, int key)
 {
-	Node* pTemp = (Node*)malloc(sizeof(Node));
+	Node* pTemp = (Node*)malloc(sizeof(Node)); // 메모리 동적 할당
+	// 값 할당
 	pTemp->key = key;
 	pTemp->left = NULL;
 	pTemp->right = NULL;
+	// 루트 노드가 비어있다면
 	if (head->left ==NULL){
-		head->left = pTemp;
+		head->left = pTemp; // 루트노드에 할당 후 함수 종료
 		return 0;
 	}
 	Node* ptr = head->left;
@@ -174,19 +173,18 @@ int insert(Node* head, int key)
 		if (ptr->key==key)
 			return 1;
 
-		parent = ptr;
+		parent = ptr; // 부모 노드 저장
 
-		if (ptr->key < key)
-			ptr = ptr->right;
-		else
-			ptr = ptr->left;
+		if (ptr->key < key) // key가 현재노드의 key보다 크다면 
+			ptr = ptr->right; // 오른쪽으로 이동
+		else // key가 현재노드의 key보다 작다면 
+			ptr = ptr->left; // 왼쪽으로 이동
 	}
 
-	if (parent->key < key)
-		parent->right = pTemp;
-	else
-		parent->left = pTemp;
-
+	if (parent->key < key) // key가 부모노드의 key보다 크다면
+		parent->right = pTemp; // 부모노드의 오른쪽에 key에 할당
+	else // key가 부모노드의 key보다 작다면 
+		parent->left = pTemp; // 부모노드의 왼쪽에 key에 할당
 }
 
 int deleteLeafNode(Node* head, int key)
@@ -206,7 +204,7 @@ int deleteLeafNode(Node* head, int key)
 
 				free(ptr); // ptr을 메모리 해제
 			} else{ // ptr이 단말노드가 아닐 때
-				printf("the node [%d] is not a leaf \n", ptr->key);
+				printf("the node [%d] is not a leaf \n", ptr->key); // 출력
 			}
 			return 1;
 		}
@@ -217,16 +215,18 @@ int deleteLeafNode(Node* head, int key)
 		else // key가 더 작을 경우
 			ptr = ptr->left; //왼쪽으로 이동
 	}
-	printf("Cannot find the node for key [%d]\n ", key);
-	return 1;
+
+	printf("Cannot find the node for key [%d]\n ", key); // 삭제할 노드를 찾지 못하였다면
+	return 1; // 삭제 실패
 }
 
 Node* searchRecursive(Node* ptr, int key)
 {
 	if(ptr==NULL) //현재 노드가 NULL이면 NULL 반환
 		return NULL;
-	if (ptr->key == key)
-		return ptr;
+
+	if (ptr->key == key) // key랑 같을 경우
+		return ptr; // 그때의 ptr 반환
 
 	if (ptr->key < key) // key가 더 클 경우
 		return searchRecursive(ptr->right,key); // 오른쪽으로 이동
@@ -238,21 +238,22 @@ Node* searchIterative(Node* head, int key)
 {
 	Node* ptr = head->left; // 루트 노드로 시작
 	while(ptr){
-		if(ptr->key==key)
-			return ptr;
+		if(ptr->key==key) // key랑 같을 경우
+			return ptr; // 그때의 ptr 반환
 			
 		if (ptr->key < key) // key가 더 클 경우
 			ptr = ptr->right; // 오른쪽으로 이동
 		else // key가 더 작을 경우
 			ptr = ptr->left; //왼쪽으로 이동
 	}
+	return NULL; // key를 찾지 못한다면 NULL반환
 }
 
 
 int freeBST(Node* head)
 {
     if (head == NULL) {
-        return 0;
+        return 0; // null이면 0반환
     }
 
     int count = 0;
@@ -260,11 +261,9 @@ int freeBST(Node* head)
     count += freeBST(head->left);
     // 오른쪽 서브트리의 노드를 먼저 해제
     count += freeBST(head->right);
-
-    // 현재 노드를 해제
-    printf("노드 %d 해제\n", head->key);
-    free(head);
-    count++;
+	
+    free(head); // 현재 노드 해제
+    count++; // 해제한 노드 수 증가
 
     return count;
 }
